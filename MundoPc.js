36b946c9eb2d96d58502338236a801cb44a979df
damarishernandez <cortezhernandezdamaris@gmail.com>
2021-11-22 19:52:53 -0500

    Adding Mundo PC

--- MundoPc.js
+++ MundoPc.js
@@ -156,25 +156,27 @@ class Order {
 let mouse1 = new Mouse("USB", "HP");
 let mouse2 = new Mouse("Bluetooth", "Dell");
 
-console.log(mouse1.toString());
-console.log(mouse2.toString());
+console.log(`${mouse1}`);
+console.log(`${mouse2}`);
 
 // object keyboard
 let keyboard1 = new Keyboard("Bluetooth", "MSI");
 let keyboard2 = new Keyboard("USB", "Acer");
 
-console.log(keyboard1.toString());
-console.log(keyboard2.toString());
+console.log(`${keyboard1}`);
+console.log(`${keyboard2}`);
 
 
 // object monitor 
 let monitor1 = new Monitor("Dell", 28);
 let monitor2 = new Monitor("Hp", 27);
-console.log(monitor1.toString());
+
+console.log(`${monitor1}`);
 
 // Object Computer
 let computer1 = new Computer("HP", monitor1, mouse1, keyboard1);
 let computer2 = new Computer("Armada", monitor2, mouse2, keyboard2);
+
 console.log(`${computer1}`);
 console.log(`${computer2}`);
 
